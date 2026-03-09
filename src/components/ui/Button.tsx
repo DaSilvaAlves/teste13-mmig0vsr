35295ac92ef703aa11f6d5e32e4d2e8dcd823c5d
Eurico Alves <euricojsalves@gmail.com>
2026-03-09 00:28:56 +0000

    feat: add src/components/ui/Button.tsx

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,15 @@
+import { HTMLAttributes } from 'react';
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const Button = ({ children, ...props }: ButtonProps) => {
+  return (
+    <button className="button" {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
